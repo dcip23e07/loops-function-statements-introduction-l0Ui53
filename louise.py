266d9cd1a23f40216e930bad54b08f50ce8a26fe
Louise <louise.gauck@dci-student.org>
2023-10-30 18:19:53 +0100

    modified

--- louise.py
+++ louise.py
@@ -1,24 +1,23 @@
-balance = 2000
-min_wd = 50
-max_wd = 1000
+balance:int = 2000
+min_wd:int = 50
+max_wd:int = 1000
 
-def ATM(withdrawal: int):
+def atm(withdrawal: int):
     choice = input("Do you want to withdraw money? Type 'yes' or 'no': ")
-    if choice == "no":
-        print("goodbye")
-        return
-    elif choice == 'yes':
-        valid_amount = False
-        while not valid_amount:
-            amount = int(input("How much? "))
-            if amount < min_wd:
-                amount = int(input("the minimum allowed is 50, please enter again: "))
-            elif amount > max_wd:
-                amount = int(input("the maximum allowed is 1000. please enter again: "))  
-            else:
-                print("thank you. please take your money")
-                break
-ATM(0)  
-    
-
-ATM(withdrawal=0)
+    choice_0 = "no"
+    choice_1 = "yes"
+    if choice == choice_0:
+        print("Goodbye")
+    elif choice == choice_1:
+        amount = int(input("Please enter amount: "))
+        if amount < min_wd:
+            amount = int(input("Please enter minimum amount of 50: "))
+        elif amount > max_wd:
+            amount = int(input("Please enter maximum amount of 1000: "))
+        elif amount > balance:
+            amount = int(input("This amount exceeds your balance. Please enter again: ")) 
+        else:
+            print("Thank you. Please take your money!")
+    else:
+        print(input("Please enter 'yes' or 'no': "))
+atm(0)  
